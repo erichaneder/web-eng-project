329ee7c4ca9ee6cd90d53fa1c4fd3b5f1135c368
Eric Haneder <eric.haneder@compax.at>
2023-11-08 15:36:13 +0100

    fixed routing in profile dropdown

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -5,7 +5,7 @@
           <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <!-- Mobile menu button-->
             <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span class="absolute -inset-0.5" />
+              <span class="absolute -inset-0.5"></span>
               <span class="sr-only">Open main menu</span>
               <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
               <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
@@ -23,7 +23,7 @@
           </div>
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <router-link to="/basket" class="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="absolute -inset-1.5" />
+              <span class="absolute -inset-1.5"></span>
               <span class="sr-only">View shopping cart</span>
               <ShoppingCartIcon class="h-8 w-8" aria-hidden="true">
               </ShoppingCartIcon>
@@ -33,7 +33,7 @@
             <Menu as="div" class="relative ml-3">
               <div>
                 <MenuButton class='relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 hover:bg-gray-800'>
-                  <span class="absolute -inset-1.5" />
+                  <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
                   <UserIcon class="h-9 w-9 text-gray-400" />
                 </MenuButton>
@@ -41,13 +41,13 @@
               <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                 <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <MenuItem v-slot="{ active }">
-                    <a href="/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
+                    <router-link to="/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</router-link>
                   </MenuItem>
                   <MenuItem v-slot="{ active }">
-                    <a href="/login" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Log in</a>
+                    <router-link to="/login" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Log in</router-link>
                   </MenuItem>
                   <MenuItem v-slot="{ active }">
-                    <a href="/register" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign up</a>
+                    <router-link to="/register" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign up</router-link>
                   </MenuItem>
                 </MenuItems>
               </transition>
